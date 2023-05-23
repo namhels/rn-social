4b83edb090bcf2e5e7d2cac24876e3bd6db3ce48
namhels <8.lsv.888@gmail.com>
2023-05-23 10:11:26 +0300

    init rn-social

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Yahoo:))))</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "purple",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  borderText: {
+    borderColor: "white",
+    borderWidth: 2,
+    borderRadius: 10,
+  },
+});
