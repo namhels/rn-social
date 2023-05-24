c65b74b2208841a3d7e5e44c4c7888bf1b1eeb6d
namhels <8.lsv.888@gmail.com>
2023-05-24 17:16:08 +0300

    add expo-app-loading

--- Screens/RegistrationScreen.js
+++ Screens/RegistrationScreen.js
@@ -14,9 +14,10 @@ import {
 } from "react-native";
 
 import * as Font from "expo-font";
-import { AppLoading } from "expo";
+import AppLoading from "expo-app-loading";
 
 const initialState = {
+  name: "",
   email: "",
   password: "",
 };
@@ -27,6 +28,33 @@ const loadApplication = async () => {
     "Roboto-Medium": require("../assets/fonts/Roboto/Roboto-Medium.ttf"),
   });
 };
+
+// export default function RegistrationScreen() {
+//   const [isReady, setIsReady] = useState(false);
+
+//   if (!isReady) {
+//     return (
+//       <AppLoading
+//         startAsync={loadApplication}
+//         onFinish={() => setIsReady(true)}
+//         onError={console.warn}
+//       />
+//     );
+//   }
+
+//   return (
+//     <View style={styles.container}>
+//       <ImageBackground
+//         style={styles.image}
+//         source={require("../assets/images/BG-2x.jpg")}
+//       >
+//         <View style={styles.header}>
+//           <Text style={styles.headerTitle}>Регистрация</Text>
+//         </View>
+//       </ImageBackground>
+//     </View>
+//   );
+// }
 
 export default function RegistrationScreen() {
   // console.log(Platform.OS);
@@ -68,73 +96,75 @@ export default function RegistrationScreen() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.borderText}>Yahoo:)</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={keyboardHide}>
+      <View style={styles.container}>
+        <ImageBackground
+          style={styles.image}
+          source={require("../assets/images/BG-2x.jpg")}
+        >
+          <KeyboardAvoidingView
+            behavior={Platform.OS == "ios" ? "padding" : "height"}
+          >
+            <View
+              style={{
+                ...styles.form,
+                marginBottom: isShowKeyboard ? 20 : 150,
+                width: dimensions,
+              }}
+            >
+              <View style={styles.header}>
+                <Text style={styles.headerTitle}>Регистрация</Text>
+              </View>
+              <View>
+                <TextInput
+                  style={styles.input}
+                  textAlign={"center"}
+                  onFocus={() => setIsShowKeyboard(true)}
+                  value={state.name}
+                  onChangeText={(value) =>
+                    setstate((prevState) => ({ ...prevState, name: value }))
+                  }
+                  placeholder="Никнейм"
+                />
+              </View>
+              <View style={{ marginTop: 20 }}>
+                <TextInput
+                  style={styles.input}
+                  textAlign={"center"}
+                  onFocus={() => setIsShowKeyboard(true)}
+                  value={state.email}
+                  onChangeText={(value) =>
+                    setstate((prevState) => ({ ...prevState, email: value }))
+                  }
+                  placeholder="Адрес электронной почты"
+                />
+              </View>
+              <View style={{ marginTop: 20 }}>
+                <TextInput
+                  style={styles.input}
+                  textAlign={"center"}
+                  secureTextEntry={true}
+                  onFocus={() => setIsShowKeyboard(true)}
+                  value={state.password}
+                  onChangeText={(value) =>
+                    setstate((prevState) => ({ ...prevState, password: value }))
+                  }
+                  placeholder="Пароль"
+                />
+              </View>
+              <TouchableOpacity
+                activeOpacity={0.8}
+                style={styles.btn}
+                onPress={keyboardHide}
+              >
+                <Text style={styles.btnTitle}>Зарегистрироваться</Text>
+              </TouchableOpacity>
+            </View>
+          </KeyboardAvoidingView>
+        </ImageBackground>
+      </View>
+    </TouchableWithoutFeedback>
   );
-
-  //   return (
-  //     <TouchableWithoutFeedback onPress={keyboardHide}>
-  //       <View style={styles.container}>
-  //         <ImageBackground
-  //           style={styles.image}
-  //           // source={require("../assets/images/BG@2x.jpg")}
-  //           source={{ uri: "https://reactjs.org/logo-og.png" }}
-  //           // style={{ width: 700, height: 700 }}
-  //         >
-  //           <KeyboardAvoidingView
-  //             behavior={Platform.OS == "ios" ? "padding" : "height"}
-  //           >
-  //             <View
-  //               style={{
-  //                 ...styles.form,
-  //                 marginBottom: isShowKeyboard ? 20 : 150,
-  //                 width: dimensions,
-  //               }}
-  //             >
-  //               <View style={styles.header}>
-  //                 <Text style={styles.headerTitle}>Регистрация</Text>
-  //               </View>
-  //               <View>
-  //                 <Text style={styles.inputTitle}>EMAIL ADDRES</Text>
-  //                 <TextInput
-  //                   style={styles.input}
-  //                   textAlign={"center"}
-  //                   onFocus={() => setIsShowKeyboard(true)}
-  //                   value={state.email}
-  //                   onChangeText={(value) =>
-  //                     setstate((prevState) => ({ ...prevState, email: value }))
-  //                   }
-  //                   placeholder="Адрес электронной почты"
-  //                 />
-  //               </View>
-  //               <View style={{ marginTop: 20 }}>
-  //                 <Text style={styles.inputTitle}>PASSWORD</Text>
-  //                 <TextInput
-  //                   style={styles.input}
-  //                   textAlign={"center"}
-  //                   secureTextEntry={true}
-  //                   onFocus={() => setIsShowKeyboard(true)}
-  //                   value={state.password}
-  //                   onChangeText={(value) =>
-  //                     setstate((prevState) => ({ ...prevState, password: value }))
-  //                   }
-  //                   placeholder="Пароль"
-  //                 />
-  //               </View>
-  //               <TouchableOpacity
-  //                 activeOpacity={0.8}
-  //                 style={styles.btn}
-  //                 onPress={keyboardHide}
-  //               >
-  //                 <Text style={styles.btnTitle}>Зарегистрироваться</Text>
-  //               </TouchableOpacity>
-  //             </View>
-  //           </KeyboardAvoidingView>
-  //         </ImageBackground>
-  //       </View>
-  //     </TouchableWithoutFeedback>
-  //   );
 }
 
 const styles = StyleSheet.create({
@@ -160,12 +190,6 @@ const styles = StyleSheet.create({
   form: {
     // marginHorizontal: 40,
   },
-  inputTitle: {
-    color: "#f0f8ff",
-    marginBottom: 10,
-    fontSize: 18,
-    fontFamily: "Roboto-Medium",
-  },
   btn: {
     borderRadius: 6,
     borderWidth: 1,
@@ -188,7 +212,7 @@ const styles = StyleSheet.create({
   btnTitle: {
     color: Platform.OS === "ios" ? "#4169e1" : "#f0f8ff",
     fontSize: 18,
-    fontFamily: "DMMono-Regular",
+    fontFamily: "Roboto-Medium",
   },
   header: {
     alignItems: "center",
@@ -200,32 +224,3 @@ const styles = StyleSheet.create({
     fontFamily: "Roboto-Medium",
   },
 });
-
-// ==========================================
-
-// export default function RegistrationScreen() {
-//   return (
-//     <View style={styles.container}>
-//       <Text style={styles.borderText}>Yahoo:)</Text>
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "purple",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-//   borderText: {
-//     paddingTop: 20,
-//     paddingBottom: 20,
-//     paddingLeft: 100,
-//     paddingRight: 100,
-//     backgroundColor: "teal",
-//     borderColor: "#ab5fff",
-//     borderWidth: 2,
-//     borderRadius: 10,
-//   },
-// });
