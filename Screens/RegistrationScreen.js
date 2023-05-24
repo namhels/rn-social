088f0b84d275f72cad23398693245e8cec3c68ab
namhels <8.lsv.888@gmail.com>
2023-05-24 11:52:12 +0300

    fix bug

--- Screens/RegistrationScreen.js
+++ Screens/RegistrationScreen.js
@@ -68,67 +68,73 @@ export default function RegistrationScreen() {
   }
 
   return (
-    <TouchableWithoutFeedback onPress={keyboardHide}>
-      <View style={styles.container}>
-        <ImageBackground
-          style={styles.image}
-          // source={require("../assets/images/BG@2x.jpg")}
-          source={{ uri: "https://reactjs.org/logo-og.png" }}
-          // style={{ width: 700, height: 700 }}
-        >
-          <KeyboardAvoidingView
-            behavior={Platform.OS == "ios" ? "padding" : "height"}
-          >
-            <View
-              style={{
-                ...styles.form,
-                marginBottom: isShowKeyboard ? 20 : 150,
-                width: dimensions,
-              }}
-            >
-              <View style={styles.header}>
-                <Text style={styles.headerTitle}>Регистрация</Text>
-              </View>
-              <View>
-                <Text style={styles.inputTitle}>EMAIL ADDRES</Text>
-                <TextInput
-                  style={styles.input}
-                  textAlign={"center"}
-                  onFocus={() => setIsShowKeyboard(true)}
-                  value={state.email}
-                  onChangeText={(value) =>
-                    setstate((prevState) => ({ ...prevState, email: value }))
-                  }
-                  placeholder="Адрес электронной почты"
-                />
-              </View>
-              <View style={{ marginTop: 20 }}>
-                <Text style={styles.inputTitle}>PASSWORD</Text>
-                <TextInput
-                  style={styles.input}
-                  textAlign={"center"}
-                  secureTextEntry={true}
-                  onFocus={() => setIsShowKeyboard(true)}
-                  value={state.password}
-                  onChangeText={(value) =>
-                    setstate((prevState) => ({ ...prevState, password: value }))
-                  }
-                  placeholder="Пароль"
-                />
-              </View>
-              <TouchableOpacity
-                activeOpacity={0.8}
-                style={styles.btn}
-                onPress={keyboardHide}
-              >
-                <Text style={styles.btnTitle}>Зарегистрироваться</Text>
-              </TouchableOpacity>
-            </View>
-          </KeyboardAvoidingView>
-        </ImageBackground>
-      </View>
-    </TouchableWithoutFeedback>
+    <View style={styles.container}>
+      <Text style={styles.borderText}>Yahoo:)</Text>
+    </View>
   );
+
+  //   return (
+  //     <TouchableWithoutFeedback onPress={keyboardHide}>
+  //       <View style={styles.container}>
+  //         <ImageBackground
+  //           style={styles.image}
+  //           // source={require("../assets/images/BG@2x.jpg")}
+  //           source={{ uri: "https://reactjs.org/logo-og.png" }}
+  //           // style={{ width: 700, height: 700 }}
+  //         >
+  //           <KeyboardAvoidingView
+  //             behavior={Platform.OS == "ios" ? "padding" : "height"}
+  //           >
+  //             <View
+  //               style={{
+  //                 ...styles.form,
+  //                 marginBottom: isShowKeyboard ? 20 : 150,
+  //                 width: dimensions,
+  //               }}
+  //             >
+  //               <View style={styles.header}>
+  //                 <Text style={styles.headerTitle}>Регистрация</Text>
+  //               </View>
+  //               <View>
+  //                 <Text style={styles.inputTitle}>EMAIL ADDRES</Text>
+  //                 <TextInput
+  //                   style={styles.input}
+  //                   textAlign={"center"}
+  //                   onFocus={() => setIsShowKeyboard(true)}
+  //                   value={state.email}
+  //                   onChangeText={(value) =>
+  //                     setstate((prevState) => ({ ...prevState, email: value }))
+  //                   }
+  //                   placeholder="Адрес электронной почты"
+  //                 />
+  //               </View>
+  //               <View style={{ marginTop: 20 }}>
+  //                 <Text style={styles.inputTitle}>PASSWORD</Text>
+  //                 <TextInput
+  //                   style={styles.input}
+  //                   textAlign={"center"}
+  //                   secureTextEntry={true}
+  //                   onFocus={() => setIsShowKeyboard(true)}
+  //                   value={state.password}
+  //                   onChangeText={(value) =>
+  //                     setstate((prevState) => ({ ...prevState, password: value }))
+  //                   }
+  //                   placeholder="Пароль"
+  //                 />
+  //               </View>
+  //               <TouchableOpacity
+  //                 activeOpacity={0.8}
+  //                 style={styles.btn}
+  //                 onPress={keyboardHide}
+  //               >
+  //                 <Text style={styles.btnTitle}>Зарегистрироваться</Text>
+  //               </TouchableOpacity>
+  //             </View>
+  //           </KeyboardAvoidingView>
+  //         </ImageBackground>
+  //       </View>
+  //     </TouchableWithoutFeedback>
+  //   );
 }
 
 const styles = StyleSheet.create({
@@ -194,3 +200,32 @@ const styles = StyleSheet.create({
     fontFamily: "Roboto-Medium",
   },
 });
+
+// ==========================================
+
+// export default function RegistrationScreen() {
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.borderText}>Yahoo:)</Text>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "purple",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   borderText: {
+//     paddingTop: 20,
+//     paddingBottom: 20,
+//     paddingLeft: 100,
+//     paddingRight: 100,
+//     backgroundColor: "teal",
+//     borderColor: "#ab5fff",
+//     borderWidth: 2,
+//     borderRadius: 10,
+//   },
+// });
